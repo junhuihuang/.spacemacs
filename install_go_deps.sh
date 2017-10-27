2922f8b55d2bf0c649632d8a44fb1a262d664c8c
junhuihuang <hjh132@gmail.com>
2017-10-27 17:54:21 +0800

    update golang tools install configure

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -4,12 +4,12 @@
 
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
-go get -u -v golang.org/x/tools/cmd/guru
-go get -u -v golang.org/x/tools/cmd/gorename
-go get -u -v golang.org/x/tools/cmd/goimports
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/guru
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/gorename
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v github.com/alecthomas/gometalinter
 go get -u -v github.com/onsi/gomega
-gometalinter --install --update
+#gometalinter --install --update
 
 # 配置 gocode
 # gocode 的配置文件在 ~/.config/gocode/config.json
