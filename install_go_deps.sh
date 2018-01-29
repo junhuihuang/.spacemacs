ee08b17a94b6653b824fa23176c140d707e928c2
junhuihuang <hjh132@gmail.com>
2018-01-29 19:33:22 +0800

    using gopkgs to improve performance

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -9,6 +9,8 @@ http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v 
 http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/goimports
 go get -u -v github.com/alecthomas/gometalinter
 go get -u -v github.com/onsi/gomega
+go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
+
 #gometalinter --install --update
 
 # 配置 gocode
