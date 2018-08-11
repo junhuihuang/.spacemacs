ed638f11667ced44bc480459d2de32bcf7818818
junhuihuang <hjh132@gmail.com>
2018-08-11 11:35:47 +0800

    update go mode

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -2,14 +2,19 @@
 
 # https://github.com/syl20bnr/spacemacs/tree/master/layers/%2Blang/go
 
-go get -u -v github.com/nsf/gocode
-go get -u -v github.com/rogpeppe/godef
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/nsf/gocode
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/rogpeppe/godef
 http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/guru
 http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/gorename
 http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/goimports
-go get -u -v github.com/alecthomas/gometalinter
-go get -u -v github.com/onsi/gomega
-go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/zmb3/gogetdoc
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/cweill/gotests/...
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/josharian/impl
+
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/alecthomas/gometalinter
+http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/onsi/gomega
 
 #gometalinter --install --update
 
