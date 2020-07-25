75afea17bcf3f3c20ef4e49245cb26d01cf1e337
junhuihuang <hjh132@gmail.com>
2020-07-25 13:22:04 +0800

    update go dependent tools

--- install_go_deps.sh
+++ install_go_deps.sh
@@ -1,24 +1,21 @@
 #!/bin/sh
 
-# https://github.com/syl20bnr/spacemacs/tree/master/layers/%2Blang/go
+# https://github.com/syl20bnr/spacemacs/tree/develop/layers/+lang/go
 
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/nsf/gocode
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/rogpeppe/godef
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/guru
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/gorename
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v golang.org/x/tools/cmd/goimports
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/zmb3/gogetdoc
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/cweill/gotests/...
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/josharian/impl
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/godoctor/godoctor
+export ALL_PROXY=socks5://127.0.0.1:1086
+export HTTPS_PROXY=http://127.0.0.1:1087
 
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/alecthomas/gometalinter
-http_proxy=http://127.0.0.1:8118 https_proxy=http://127.0.0.1:8118 go get -u -v github.com/onsi/gomega
-
-#gometalinter --install --update
-
-# 配置 gocode
-# gocode 的配置文件在 ~/.config/gocode/config.json
-gocode set autobuild true
+GO111MODULE=on go get -v golang.org/x/tools/gopls@latest
+GO111MODULE=on CGO_ENABLED=0 go get -v -trimpath -ldflags '-s -w' github.com/golangci/golangci-lint/cmd/golangci-lint
+go get -u -v golang.org/x/tools/cmd/godoc
+go get -u -v golang.org/x/tools/cmd/goimports
+go get -u -v golang.org/x/tools/cmd/gorename
+go get -u -v golang.org/x/tools/cmd/guru
+go get -u -v github.com/cweill/gotests/...
+go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
+go get -u -v github.com/fatih/gomodifytags
+go get -u -v github.com/godoctor/godoctor
+go get -u -v github.com/haya14busa/gopkgs/cmd/gopkgs
+go get -u -v github.com/josharian/impl
+go get -u -v github.com/rogpeppe/godef
+go get -u -v github.com/zmb3/gogetdoc
